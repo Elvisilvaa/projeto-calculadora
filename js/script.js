94d74452fa65b3cf9921ad3b42a4a5c75cf34749
Elvisilvaa <elvisfj@gmail.com>
2023-01-08 17:53:43 -0300

    Finalizando calculadora

--- js/script.js
+++ js/script.js
@@ -13,5 +13,12 @@ function clean() {
 function backspace() {
     if(resultado.textContent){
         let result = document.getElementById('resultado').innerHTML
+        resultado.innerHTML = result.substring(0, result.length -1);
+    }
+}
+
+function confirma() {
+    if(resultado.textContent != 'Erro') {
+        document.getElementById("resultado").innerHTML = eval(resultado.innerHTML)
     }
 }
